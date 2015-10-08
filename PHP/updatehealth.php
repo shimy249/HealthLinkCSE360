df11baa367a0099af01be6b17b4384df1cace208
luiscarbaj <la.carbajal92@gmail.com>
2015-10-08 14:23:01 -0700

    Update updatehealth.php

--- PHP/updatehealth.php
+++ PHP/updatehealth.php
@@ -10,19 +10,25 @@ if($sql)
 {
     mysqli_select_db($sql,'IPIMS');
     var $i = 0; 
-    while(true)
+    if (isset($_POST['Submit']))
     {
+        while(true)
+        {
         
-
-        if(isset($_POST["symptom".$i]) && !empty($_POST["symptom".$i])) {
+            if(isset($_POST["symptom".$i]) && !empty($_POST["symptom".$i])) {
             $symptoms = $_POST['symptoms'.$i];
             $severity = $_POST['severity'.$i];
             $additional = $_POST['additional'.$i];
             $insert = "INSERT INTO healthcondition (symptoms, severity, additional)
              VALUES ('$symptoms', '$severity', '$additional')";
              $i++;
+            }
         }
     }
+    else if (isset($_POST['Get_diagnosis']))
+         header('Location: /url/to/the/other/page');
+    else if (isset($_POST['Appointment']))
+        header('Location: /url/to/the/other/page');
 
 }
 else
