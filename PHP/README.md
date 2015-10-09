a08f102d0c7a4c8a718c54fde6a68a2618498aa2
aehowell <howell.ashley17@gmail.com>
2015-10-08 21:01:19 -0700

    Code for schedule appointment

--- PHP/README.md
+++ PHP/README.md
@@ -1,2 +1,1 @@
 ##backend code goes here
- 
---
+++ PHP/scheduleappointments.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ashley
+ * Date: 10/8/2015
+ * Time: 8:58 PM
+ */
+
+$sql = mysqli_connect('localhost' , 'username' , 'password');
+
+if($sql)
+{
+    echo "Connect successfully";
+
+    if (isset($_POST['doctor']))
+    {
+        mysqli_select_db($sql, 'IPIMS');
+
+        while(true) {
+
+            if(isset($_POST["time and date" . $i] ) && !empty($_POST["time and date" . $i]))
+            {
+                $time = $_POST['time' . $i];
+                $date = $_POST['date' . $i];
+                $insert = "INSERT INTO appointments (time, date, doctor) Values ('$time', '$date', '$doctor')";
+                $i++;
+            }
+        }
+
+    }
+}
+
+else
+    echo 'Connect failed';
+
+?>
