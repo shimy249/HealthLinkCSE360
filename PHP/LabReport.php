bc486129f8f06054898e1bed78157e01b4bace28
luiscarbaj <la.carbajal92@gmail.com>
2015-10-29 21:34:11 -0700

    Update LabReport.php

--- PHP/LabReport.php
+++ PHP/LabReport.php
@@ -2,8 +2,7 @@
 $conn = new mysqli('localhost', 'appbfdlk', 'ohDAUdCL4AQZ0', 'appbfdlk_HealthLinkCSE360');
 $sql = "SELECT * FROM UserData WHERE _id='".$row["PatientID"]."'";
 $result = $conn->query($sql);
-if($result->num_rows>0) {
-    while ($row = $result->fetch_assoc())
-        echo '<option value="' . $row['_id'] . '">' . $row["FirstName"] .' '. $row["LastName"]. $row["doctor"]. $row["title"]. "</option>";
-}
+while ($row = $result->fetch_assoc())
+    echo '<option value="' . $row['_id'] . '">' . $row["FirstName"] .' '. $row["LastName"]. $row["doctor"]. $row["title"]. "</option>";
+
 ?>
