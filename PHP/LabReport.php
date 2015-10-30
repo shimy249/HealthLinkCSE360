1a35477620ab76b854e122f97c579d7b83f6a2f0
luiscarbaj <la.carbajal92@gmail.com>
2015-10-29 21:21:57 -0700

    Create LabReport.php

---
+++ PHP/LabReport.php
@@ -0,0 +1,9 @@
+<?php
+$conn = new mysqli('localhost', 'appbfdlk', 'ohDAUdCL4AQZ0', 'appbfdlk_HealthLinkCSE360');
+$sql = "SELECT * FROM UserData WHERE Type = 0";
+$result = $conn->query($sql);
+if($result->num_rows>0) {
+    while ($row = $result->fetch_assoc())
+        echo '<option value="' . $row['_id'] . '">' . $row["FirstName"] .' '. $row["LastName"]. $row["doctor"]. $row["title"]. "</option>";
+}
+?>
