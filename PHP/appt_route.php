fddd9014c32cc7dd7da00b4838103655ef9f461f
rdmcclos <ryan1767@live.com>
2015-11-17 22:57:16 -0700

    fixed stuff

--- PHP/appt_route.php
+++ PHP/appt_route.php
@@ -10,7 +10,10 @@ session_start();
 $conn = new mysqli('localhost', 'appbfdlk', 'ohDAUdCL4AQZ0', 'appbfdlk_HealthLinkCSE360');
 $_SESSION['diseaseID'] =  $_POST['selected_disease'];
 $diseaseID = $_POST['selected_disease'];
+$userID = $_SESSION['userID'];
 $url = '';
+$ts = getdate();
+$date = $ts[year].'-'.$ts[mon].'-'.$ts[mday];
 if ($diseaseID > 0){
 
     $sql = "SELECT * FROM DiseaseDefinitions WHERE _id = '".$diseaseID."'";
@@ -22,6 +25,12 @@ if ($diseaseID > 0){
     }
     else { //general condition
         $url = 'appt_general.php?';
+    }
+    $symptomList = $row['SymptomList'];
+    $aSymptoms = explode(", ", $symptomList);
+    foreach ($aSymptoms as $i){
+        $sql = "INSERT INTO Conditions(PatientID, Symptom,Severity, Notes, Date) VALUES ('$userID', '$i', 3, 'Added by appointment tool', '$date')";
+        $conn->query($sql);
     }
 }else{ //no condition
     $url = 'appt_general.php?';
